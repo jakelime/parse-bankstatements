d2ab7a11c18aa9ca91e473335f1ac8fac7425a41
jakelime <jiahao89sg@gmail.com>
2023-10-23 12:41:16 +0800

    identifier ready
    cp

--- pbsm/dbs.py
+++ pbsm/dbs.py
@@ -1,15 +1,14 @@
 import fnmatch
 import os
-import re
-from pathlib import Path
 import datetime
-from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
 
 import tabula
 from pypdf import PdfReader
 import pandas as pd
 import dotenv
-import regex_spm
+
 
 if __name__ == "__main__":
     import utils
@@ -17,18 +16,30 @@ else:
     from pbsm import utils
 
 
-dotenv.load_dotenv()
-
 APP_NAME = "pbsm"
 AREA_PAYLAH_HEADER = [18.96, 7.34, 24.28, 93.47]
 POSB_CREDIT_CARD_NUMBER = os.getenv("POSB_CREDIT_CARD_NUMBER")
 
+dotenv.load_dotenv()
 lg = utils.init_logger(APP_NAME)
+
+
+class Stm(Enum):
+    """Enum representing statement types"""
+
+    UNKNOWN = "UnknownStatement"
+    DBS_CASHBACK = "DBSCashbackStatement"
+    DBS_CREDITCARD = "DBSCreditCardStatement"
+    DBS_PAYLAH = "DBSPaylahStatement"
+    DBS_ACCOUNT = "DBSAccountsStatement"
+    UOB_CREDITCARD = "UOBCreditCardStatement"
+    UOB_ACCOUNT = "UOBAccountsStatement"
 
 
 class PdfStatement:
     def __init__(self, filepath: Path):
         self.filepath = filepath
+        self.prefix = "nil"
 
     def parse_pdf(self, area: list[float]) -> pd.DataFrame:
         dfs = tabula.io.read_pdf(
@@ -54,76 +65,49 @@ class PdfStatement:
         dt_obj = datetime.datetime.strptime(dt_str, "%d %b %Y")
         return dt_obj.strftime("%Y%m%d")
 
-
-class DbsStatement(PdfStatement):
-    def __init__(self, filepath: Path):
-        super().__init__(filepath)
-        self.statement = None
-        self.statement_type = self.identify_statement_type()
-        match self.statement_type:
-            case "creditCard":
-                lg.warning("creditCard function not ready")
-            case "cashback":
-                lg.warning("cashback function not ready")
-            case "statementOfAccounts":
-                lg.warning("statementOfAccounts function not ready")
-            case "paylah":
-                self.statement = PaylahStatement(self.filepath)
-            case _:
-                raise NotImplementedError("unknown statement type")
-
-    def identify_statement_type(self) -> str:
+    def get_statement_type(self) -> Stm:
         # Filename patter matching
         if fnmatch.fnmatch(self.filepath.name, "PDF文档*.pdf"):
-            return "paylah"
-        # TODO: add more file patterns
+            return Stm.DBS_PAYLAH
 
+        # Pdf file content keywords search
         raw_txt = self.parse_pdf_to_txt()
         txt = raw_txt[:1000]
-        if POSB_CREDIT_CARD_NUMBER:
-            if POSB_CREDIT_CARD_NUMBER in txt:
-                return "creditCard"
-        elif "POSB Cashback Bonus Statement" in txt:
-            return "cashback"
-        elif "Current and Savings Account Total" in txt:
-            return "statementOfAccounts"
+        if "POSB Cashback Bonus Statement" in txt:
+            return Stm.DBS_CASHBACK
+        elif (
+            ("POSB everyday CARD NO.:" in txt)
+            and (POSB_CREDIT_CARD_NUMBER)
+            and (POSB_CREDIT_CARD_NUMBER in txt)
+        ):
+            return Stm.DBS_CREDITCARD
+        elif "Current and Savings Account" in txt:
+            return Stm.DBS_ACCOUNT
         elif "PayLah!" in txt:
-            return "paylah"
+            return Stm.DBS_PAYLAH
 
-        return "unknown"
-
-    def rename_consolidated_statements(self):
-        v = self.filepath.stem
-        v = v.replace(" ", "")
-        prefix = v.split("-")[0].replace(
-            "ConsolidatedStatement", "OCBC_ConsolidatedStatement"
-        )
-        datestr = "-".join(v.split("-")[1:])
-        dateobj = datetime.datetime.strptime(datestr, "%b-%y")
-        newname = f"{prefix}-{dateobj.strftime('%Y_%m')}{self.filepath.suffix}"
-        self.filepath = self.filepath.rename(newname)
-        lg.info(f"renamed to '{self.filepath.name}'")
-
-
-class PaylahStatement(PdfStatement):
-    def __init__(self, filepath: Path):
-        super().__init__(filepath=filepath)
+        return Stm.UNKNOWN
 
     def rename_filename(self) -> None:
-        prefix = "paylahStatement"
         dt_str = self.get_datetime_str()
-        new_name = f"{prefix}-{dt_str}{self.filepath.suffix}"
+        new_name = f"{self.prefix}-{dt_str}{self.filepath.suffix}"
         self.filepath = self.filepath.rename(new_name)
         lg.info(f"renamed to '{self.filepath.name}'")
 
 
+class DbsPaylahStatement(PdfStatement):
+    def __init__(self, filepath: Path):
+        super().__init__(filepath)
+        self.prefix = Stm.DBS_PAYLAH
+
+
 def main():
     pathfinder = utils.PathFinder()
-    files = pathfinder.get_pdf_files()
-    [print(x) for x in files]
 
-    stm = DbsStatement(filepath=files[1])
-    print(stm.identify_statement_type())
+    for fp in pathfinder.get_pdf_files():
+        statement = PdfStatement(filepath=fp)
+        stm_type = statement.get_statement_type()
+        lg.info(f"{fp.stem} is {stm_type}")
 
 
 if __name__ == "__main__":
